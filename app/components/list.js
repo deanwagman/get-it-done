3c08d8b7a1e9de5e60ee21a2923e1508d6bf9f91
Dean Wagman <dean@purplerockscissors.com>
2016-07-19 22:37:48 -0400

    Initial Commit

---
+++ app/components/list.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+Class List extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { items: [
+      { text: 'Clean living room', completed: false },
+      { text: 'Clean Bathroom', completed: false },
+      { text: 'Organize Magic Cards', completed: false },
+      { text: 'Finish App', completed: false },
+      { text: 'Water Plant', completed: false }
+    ] };
+  }
+  render() {
+    return (
+
+    );
+  }
+}
+
+export default List;
